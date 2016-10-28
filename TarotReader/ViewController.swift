5e251ae0b56cec16895bd9cf0c3b5e5e8c225ec3
Scott Richards <scott.srichards@gmail.com>
2016-10-28 14:29:36 -0700

    Initial Commit

---
+++ TarotReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TarotReader
+//
+//  Created by Scott Richards on 10/28/16.
+//  Copyright © 2016 Scott Richards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
